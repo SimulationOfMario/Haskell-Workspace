ae810008e40f5dfdac1dfab1127be797a98475c6
SimulationOfMario <0610921089@uma.es>
2023-10-04 20:56:31 +0200

    Chater 1 Update

--- Real-World-Haskell/Chapter 1 - Getting Started/WordCount.hs
+++ Real-World-Haskell/Chapter 1 - Getting Started/WordCount.hs
@@ -1,4 +1,7 @@
--- Simple word count program
+-- Simple count program
 
 main = interact wordCount
     where wordCount input = show (length (lines input)) ++ "\n"
+
+-- Type in shell: runghc WordCount < quux.txt 
+-- Output: 7
