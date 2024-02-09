2b1e267c8272062f61f65e6e69972bc2e3ceee18
SimulationOfMario <imthedarkestspawn@gmail.com>
2024-02-09 20:51:50 +0100

    More details

--- Real-World-Haskell/Chapter5-Writing-a-Library-and-JSON-Data/mypretty/Setup.hs
+++ Real-World-Haskell/Chapter5-Writing-a-Library-and-JSON-Data/mypretty/Setup.hs
@@ -1,8 +1,8 @@
-#!/usr/bin/env runhaskell
-import Distribution.Simple
+-- #!/usr/bin/env runhaskell
+-- import Distribution.Simple
 
-main :: IO ()
-main = defaultMain
+-- main :: IO ()
+-- main = defaultMain
 
 -- ? This file allows Cabal's build process to be heavily customized (if a package needs it)
 -- ? This file is the simplest setup
