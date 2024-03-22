77947c0ed44b50c219d2931a7abaf1b4caf88a4e
SimulationOfMario <imthedarkestspawn@gmail.com>
2024-03-22 21:36:58 +0100

    More

---
+++ Real-World-Haskell/Chapter6-Using-Typeclasses/JSONClass.hs
@@ -0,0 +1,13 @@
+-- ! For execute this file you should write:
+-- ! For ghci: 
+import SimpleJSON
+
+type JSONError = String
+
+class JSON a where
+    toJValue :: a -> JValue
+    fromJValue :: JValue -> Either JSONError a
+
+instance JSON JValue where
+    toJValue = id
+    fromJValue = Right
